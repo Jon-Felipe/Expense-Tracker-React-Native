11d73015ec80fe7db07deeb74dc255df1bb6aa6b
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-31 12:40:07 +0200

    icon button styles

--- components/UI/IconButton.js
+++ components/UI/IconButton.js
@@ -4,12 +4,24 @@ import { Ionicons } from '@expo/vector-icons';
 
 export default function IconButton({ icon, size, color, onPress }) {
   return (
-    <Pressable onPress={onPress}>
-      <View>
+    <Pressable
+      style={({ pressed }) => pressed && styles.pressed}
+      onPress={onPress}
+    >
+      <View style={styles.buttonContainer}>
         <Ionicons name={icon} size={size} color={color} />
       </View>
     </Pressable>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 24,
+    padding: 6,
+    margin: 8,
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
